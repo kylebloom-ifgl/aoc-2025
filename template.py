83fd392086e7dd1726da7c704ad5c12a67a4493d
Kyle Bloom <Kyle.Bloom@ifg-ltd.com>
2025-12-01 13:28:36 +0000

    Initial commit

---
+++ template.py
@@ -0,0 +1,44 @@
+from argparse import ArgumentParser
+from pathlib import Path
+
+
+def parse(str_data):
+    pass
+
+
+def part1(data):
+    pass
+
+
+def part2(data):
+    pass
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument(
+        "input",
+        default=Path("./input.txt"),
+        type=Path,
+        nargs="?",
+        help="Path to the input file",
+    )
+    parser.add_argument(
+        "-p", "--part", default="both", choices=["both", "part1", "part2"]
+    )
+    args = parser.parse_args()
+
+    with open(args.input, "r") as input_file:
+        file_data = input_file.read()
+
+    data = parse(file_data)
+
+    if args.part in ["both", "part1"]:
+        part1(data)
+
+    if args.part in ["both", "part2"]:
+        part2(data)
+
+
+if __name__ == "__main__":
+    main()
